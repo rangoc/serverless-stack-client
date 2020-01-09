104bdfd2f61fde13cd9a16407ee8212287580958
Goran <cabarkapa.goran@gmail.com>
2020-01-09 16:07:36 +0100

    Added Signup form

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containters/Home";
 import NotFound from "./containters/NotFound";
+import Login from "./containters/Login";
+import AppliedRoute from "./components/AppliedRoute";
+import Signup from './containters/Signup';
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      { /* Finally, catch all unmatched routes */ }
-      <Route component={NotFound} />
-    </Switch>
-  );
+export default function Routes({ appProps }) {
+    return (
+      <Switch>
+        <AppliedRoute path="/" exact component={Home} appProps={appProps} />
+        <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
+        <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
+        { /* Finally, catch all unmatched routes */ }
+        <Route component={NotFound} />
+      </Switch>
+    );
 }
