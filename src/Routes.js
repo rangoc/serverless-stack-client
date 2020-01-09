437461b5e7dae1509f9d49a4fea03ff071952a17
Goran <cabarkapa.goran@gmail.com>
2020-01-09 14:33:14 +0100

    Added and set up AWS Amplify

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containters/Home";
+import NotFound from "./containters/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
+      { /* Finally, catch all unmatched routes */ }
+      <Route component={NotFound} />
     </Switch>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-central-1",
+      BUCKET: "notes-serverlessstack-uploads"
+    },
+    apiGateway: {
+      REGION: "eu-central-1",
+      URL: "https://dkb2j4ynyb.execute-api.eu-central-1.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "eu-central-1",
+      USER_POOL_ID: "eu-central-1_ct2StzqdC",
+      APP_CLIENT_ID: "339qf867q4aufr412kl203v2om",
+      IDENTITY_POOL_ID: "eu-central-1:c2d09bd9-fb52-4a25-9783-a3e9752b2560"
+    }
+  };
